9d0e1917a9dbf6134ce111dbc096465aeede9905
Jared Kells <jared@criticalarc.com>
2016-11-17 13:09:07 +1100

    Example console application

---
+++ ConsoleApplication/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Threading.Tasks;
+using Microsoft.AspNet.SignalR.Client;
+
+namespace ConsoleApplication
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            // This is a demo, give the web project long enough to start.
+            RunAsync().Wait();
+            Console.WriteLine("Done");
+            Console.ReadKey();
+        }
+
+        private static async Task RunAsync()
+        {
+            var tcs = new TaskCompletionSource<object>();
+
+            var connection = new HubConnection("http://localhost:5000/signalr/hubs");
+            connection.StateChanged +=
+                change =>
+                {
+                    Console.WriteLine($"StateChanged: {change.NewState}. Transport: {connection.Transport?.Name}");
+                };
+
+            var hub = connection.CreateHubProxy("MyHub");
+            hub.On("helloWorld", () =>
+            {
+                Console.WriteLine("Hello World Called.");
+                tcs.SetResult(null);
+            });
+
+            await connection.Start();
+            await tcs.Task;
+        }
+    }
+}
